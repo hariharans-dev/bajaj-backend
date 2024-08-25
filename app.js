4d8b527b90d1c5de18b2d38d96ca33c064b18477
shariharan182003 <shariharan182003@gmail.com>
2024-08-25 11:45:32 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');  // Import cors
+
 const app = express();
 
+app.use(cors());  // Use cors
 app.use(express.json());
 
 function getHighestLowercaseAlphabet(alphabets) {
